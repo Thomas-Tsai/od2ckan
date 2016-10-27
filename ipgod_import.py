53e7b1b88b8e69625963c091a6316831fdacdb32
Thomas Tsai <tlinux.tsai@gmail.com>
2016-10-27 22:46:29 +0800

    try to add import daemon code

--- ipgod_import.py
+++ ipgod_import.py
@@ -1,16 +1,29 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+import odtw
+import map2ckan
+import os
+import logging
 import psycopg2
+import time
 from datetime import datetime
+
+LOGGING_FILE = 'ipgod-od2ckan.log'
+logging.basicConfig(filename=LOGGING_FILE,
+                    level=logging.INFO,
+                    format='%(asctime)s [%(levelname)s] %(filename)s_%(lineno)d  : %(message)s')
+logger = logging.getLogger('root') 
 
 class ipgoddb():
     def __init__(self)
-        try:  
+        try:
             self.conn = psycopg2.connect("dbname='ipgod' user='thomas' host='localhost' password='okok7480'")
             self.cur = conn.cursor()
         except:
             print "connect db error"
 
     def get_pkgs(self):
-        try:  
+        try:
             self.cur.execute("SELECT package_name, status, datetime from import where  datetime > CURRENT_TIMESTAMP - INTERVAL '60 secs' and status == 0")
             rows = self.cur.fetchall()
         except:
@@ -21,25 +34,47 @@ class ipgoddb():
         return pkgs
 
     def update_pkg(self, package, status):
-        try:  
+        try:
             self.cur.execute("UPDATE import SET status = %s where package_name = %s", (statue, package))
             self.conn.commit()
         except:
             print "update error"
 
     def log_package(self, package, log):
-        try:  
+        try:
             self.cur.execute("UPDATE import SET comment = %s where package_name = %s", (log, package))
             self.conn.commit()
         except:
             print "log comment error"
 
-    def remove_pkgs(self):
-        try:  
+    def remove_pkg(self):
+        try:
             self.cur.execute("DELETE from import  where package_name = %s", package)
             self.conn.commit()
         except:
             print "remove error"
 
+if __name__ == '__main__':
+    idb = ipgoddb()
+    while True:
+        pkgs = idb.get_pkgs()
+        for pkg in pkgs:
 
-class importd():
+            jsonfile = pkg['package_name']
+            odtw = odtw.od()
+            data = odtw.read(jsonfile)
+
+            ckmap = map2ckan.mapod2ckan()
+            package = ckmap.map(data) 
+            od_data_path = os.path.dirname(os.path.realpath(jsonfile))
+            package['basepath'] = od_data_path
+            put2ckan = import2ckan()
+            res = put2ckan.commit(package)
+            if res == True:
+                idb.update_pkg(pkg, res)
+                idb.remove_pkg(pkg)
+            else:
+                idb.update_pkg(pkg, res)
+                idb.log_package(pkg, error_log)
+    time.sleep(10)
+
